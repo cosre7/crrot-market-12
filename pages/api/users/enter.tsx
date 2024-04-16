77fea34e20549345adcccd3e9f3a2c1c3853c46d
cosre7 <cosre7@gmail.com>
2024-04-16 17:08:19 +0900

    12버전으로 변경

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,13 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string;
-};
-
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: "John Doe" });
+  if(req.method != 'POST') {
+    res.status(401).end();
+  }
+  console.log(req.body.email);
+  res.status(200).end();
 }
