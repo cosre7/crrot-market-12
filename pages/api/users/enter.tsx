d1d8e157852714c9808097803ca422c233023ee4
cosre7 <cosre7@gmail.com>
2024-04-16 17:39:55 +0900

    [feat] useMutation 훅 사용

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,13 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if(req.method != 'POST') {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method != 'POST') {
     res.status(401).end();
   }
-  console.log(req.body.email);
-  res.status(200).end();
+  console.log(req.body);
+  res.json({ OK: true });
 }
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -4,6 +4,7 @@ import { useForm } from 'react-hook-form';
 import Button from '../components/button';
 import Input from '../components/input';
 import { cls } from '../libs/client/utils';
+import useMutation from '@/libs/client/useMutation';
 
 interface EnterForm {
   email?: string;
@@ -11,6 +12,7 @@ interface EnterForm {
 }
 
 const Enter: NextPage = () => {
+  const [enter, { loading, data, error }] = useMutation('/api/users/enter');
   const [submitting, setSubmitting] = useState(false);
   const { register, handleSubmit, reset } = useForm<EnterForm>();
   const [method, setMethod] = useState<'email' | 'phone'>('email');
@@ -22,18 +24,10 @@ const Enter: NextPage = () => {
     reset();
     setMethod('phone');
   };
-  const onValid = (data: EnterForm) => {
-    setSubmitting(true);
-    fetch('/api/users/enter', {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(() => {
-      setSubmitting(false);
-    });
+  const onValid = (validForm: EnterForm) => {
+    enter(validForm);
   };
+  console.log(loading, data, error);
   return (
     <div className='mt-16 px-4'>
       <h3 className='text-3xl font-bold text-center'>Enter to Carrot</h3>
