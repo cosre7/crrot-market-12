95941a675e4ac99e9e52e1f41ddae8cb261b9d5d
cosre7 <cosre7@gmail.com>
2024-04-21 06:51:02 +0900

    [feat] token 회원가입

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -5,91 +5,25 @@ import client from '@libs/server/client';
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
   const payload = phone ? { phone } : { email };
-  // upsert, 동적 방식으로 구현
-  const user = await client.user.upsert({
-    where: {
-      // ...(phone && { phone }),
-      // ...(email && { email })
-      ...payload
-    },
-    create: {
-      name: 'Anonymous',
-      // ...(phone && { phone }),
-      // ...(email && { email })
-      ...payload
-    },
-    update: {}
+  const token = await client.token.create({
+    data: {
+      payload: '12345',
+      user: {
+        // 조건과 만족하는 user가 있는 경우 token과 연결, 없을 경우 user 생성
+        connectOrCreate: {
+          where: {
+            ...payload
+          },
+          create: {
+            name: 'Anonymous',
+            ...payload
+          }
+        }
+      }
+    }
   });
-  console.log(user);
+  console.log(token);
 
-  // upsert 방식으로 구현
-  // let user;
-  // if (phone) {
-  //   user = await client.user.upsert({
-  //     where: {
-  //       phone
-  //     },
-  //     create: {
-  //       phone,
-  //       name: 'Anonymous'
-  //     },
-  //     update: {}
-  //   });
-  // } else if (email) {
-  //   user = await client.user.upsert({
-  //     where: {
-  //       email
-  //     },
-  //     create: {
-  //       email,
-  //       name: 'Anonymous'
-  //     },
-  //     update: {}
-  //   });
-  // }
-
-  // upsert 사용 전
-  // let user;
-  // if (email) {
-  //   // 이메일을 가지는 유저 찾기
-  //   user = await client.user.findUnique({
-  //     where: {
-  //       email
-  //     }
-  //   });
-  //   if (user) console.log('found it');
-  //   // 유저가 없을 경우 만들기
-  //   if (!user) {
-  //     console.log('Did not found. Will create.');
-  //     user = await client.user.create({
-  //       data: {
-  //         name: 'Anonymous',
-  //         email
-  //       }
-  //     });
-  //   }
-  //   console.log(user);
-  // }
-  // if (phone) {
-  //   // 폰을 가지는 유저 찾기
-  //   user = await client.user.findUnique({
-  //     where: {
-  //       phone
-  //     }
-  //   });
-  //   if (user) console.log('found it');
-  //   // 유저가 없을 경우 만들기
-  //   if (!user) {
-  //     console.log('Did not found. Will create.');
-  //     user = await client.user.create({
-  //       data: {
-  //         name: 'Anonymous',
-  //         phone
-  //       }
-  //     });
-  //   }
-  //   console.log(user);
-  // }
   return res.status(200).end();
 }
 
