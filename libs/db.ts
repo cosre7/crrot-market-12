71a8da58ee68fc65c7ca05fdce1605a80db04b6f
cosre7 <cosre7@gmail.com>
2024-04-21 00:42:53 +0900

    [feat] User 삭제 시 옵션 SMSToken에 추가

--- libs/db.ts
+++ libs/db.ts
@@ -2,28 +2,4 @@ import { PrismaClient } from '@prisma/client';
 
 const db = new PrismaClient();
 
-async function test() {
-  // const token = await db.sMSToken.create({
-  //   data: {
-  //     token: '121212',
-  //     user: {
-  //       connect: {
-  //         id: 1
-  //       }
-  //     }
-  //   }
-  // });
-  const token = await db.sMSToken.findUnique({
-    where: {
-      id: 1
-    },
-    include: {
-      user: true
-    }
-  });
-  console.log(token);
-}
-
-test();
-
 export default db;
