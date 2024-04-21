a8ec3afe8211837ff4fbf85a8fdb2871e26175fc
cosre7 <cosre7@gmail.com>
2024-04-21 17:07:43 +0900

    [feat] 관련 상품

--- pages/api/products/[id].ts
+++ pages/api/products/[id].ts
@@ -5,6 +5,7 @@ import { withApiSession } from '@libs/server/withSession';
 
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
   const { id } = req.query;
+  if (!id) return res.status(400).json({ ok: false });
   const product = await client.product.findUnique({
     where: {
       id: +id.toString()
@@ -19,7 +20,23 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) 
       }
     }
   });
-  res.json({ ok: true, product });
+  const terms = product?.name.split(' ').map((word) => ({
+    name: {
+      contains: word
+    }
+  }));
+  const relatedProducts = await client.product.findMany({
+    where: {
+      OR: terms,
+      AND: {
+        id: {
+          not: product?.id
+        }
+      }
+    }
+  });
+  console.log(relatedProducts);
+  res.json({ ok: true, product, relatedProducts });
 }
 
 export default withApiSession(
