104bdf7cadc45b9a2593ffc715e14d7e7b40e515
cosre7 <cosre7@gmail.com>
2024-04-21 14:37:11 +0900

    [refactor] public 화면과 private 화면 구분

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -37,4 +37,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) 
   return res.json({ ok: true });
 }
 
-export default withHandler('POST', handler);
+export default withHandler({
+  method: 'POST',
+  handler
+});
